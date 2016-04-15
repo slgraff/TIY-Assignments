336b290fdaeff78bbdfac693092226f864e84fcd
slgraff <slgraff@gmail.com>
2016-04-15 10:35:49 -0400

    Completed FizzBuzz implementation, put in function
    Completed the implementation of the FizzBuzz problem, moved the code
    into its own function outside of main(), called from main.

--- CodeKata1/CodeKata1/main.m
+++ CodeKata1/CodeKata1/main.m
@@ -8,50 +8,82 @@
 
 #import <Foundation/Foundation.h>
 
+// Declaration of function FizzBuzz
+void FizzBuzz(void);
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
         
-        
-        
-        /*
-        FizzBuzz - Multiples of 3 and 5
-        If we list all the natural numbers below 10 that are multiples of 3 or 5 we get 3, 5, 6 and 9.
-        The sume of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
-        */
-        
-        // Initialize the sum variable
-        int multiSum = 0;
-        
-        // Initialize a boolean for whether or not to print each number
-        BOOL printNum = NO;
-        
-        // Loop through numbers from 1 to 1000, exclusive. Determine multiples of 3 or 5, calculate
-        // sum of those numbers
-        for (int theNum = 1; theNum < 1000; theNum += 1){
-            
-            // Is number a multiple of 3
-            if (theNum % 3 == 0) {
-                multiSum += theNum;
-                
-                if (printNum) {
-                    NSLog(@"%d is a multiple of 3", theNum);
-                }
-            } else if (theNum % 5 == 0) {
-                multiSum += theNum;
-                
-                if(printNum) {
-                    NSLog(@"%d is a multiple of 5", theNum);
-                }
-            }
-        }
-        // Print the sum of numbers to the log
-        NSLog(@"The sum of the numbers is %d", multiSum);
-        
-        
-        
+        FizzBuzz();
         
     }
     return 0;
+}
+
+
+/*
+ FizzBuzz
+ Multiples of 3 and 5
+ If we list all the natural numbers below 10 that are multiples of 3 or 5 we get 3, 5, 6 and 9.
+ The sume of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+void FizzBuzz(){
+
+    
+    // Initialize the sum variable
+    int multiSum = 0;
+    
+    // Set the maximum number
+    int maxNum = 1000;
+    
+    // Set the desired multiples
+    int fizz = 3;
+    int buzz = 5;
+    
+    // Initialize boolean for whether or print, FIZZBUZZ
+    BOOL printNum = NO;
+    BOOL fizzBuzz = YES;
+    
+    // Loop through numbers, find multiples of 3 or 5, calculate sum
+    for (int theNum = 1; theNum < maxNum; theNum += 1){
+        
+        // Is number a multiple of 3
+        if (theNum % fizz == 0 && theNum % buzz == 0) {
+            multiSum += theNum;
+            
+            if (printNum) {
+                NSLog(@"%d is a multiple of %d", theNum, fizz);
+            }
+            
+            if (fizzBuzz) {
+                NSLog(@"FIZZBUZZ");
+            }
+            
+        } else if (theNum % fizz == 0) {
+                multiSum += theNum;
+                
+                if(printNum) {
+                    NSLog(@"%d is a multiple of %d", theNum, buzz);
+                }
+                
+                if (fizzBuzz) {
+                    NSLog(@"FIZZ");
+                }
+        } else if (theNum % buzz == 0) {
+            multiSum += theNum;
+            
+            if(printNum) {
+                NSLog(@"%d is a multiple of %d", theNum, buzz);
+            }
+            
+            if (fizzBuzz) {
+                NSLog(@"BUZZ");
+            }
+        }
+    }
+    // Print the sum of numbers to the log
+    NSLog(@"The sum of the numbers is %d", multiSum);
 }
