b6f3465ce0522e75e5fc6f3636ae4072af91423d
slgraff <slgraff@gmail.com>
2016-04-15 16:04:52 -0400

    Added palindrome function
    Added function to calculate largest palindrome for product of 3-digit
    numbers.

--- CodeKata1/CodeKata1/main.m
+++ CodeKata1/CodeKata1/main.m
@@ -8,16 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-// Declaration of function FizzBuzz
+// Declaration of functions FizzBuzz and Palindrome
 void FizzBuzz(void);
+void Palindrome(void);
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
         
         FizzBuzz();
+        Palindrome();
         
     }
     return 0;
@@ -37,7 +37,7 @@ void FizzBuzz(){
     int multiSum = 0;
     
     // Set the maximum number
-    int maxNum = 1000;
+    int ceilingNum = 1000;
     
     // Set the desired multiples
     int fizz = 3;
@@ -48,7 +48,7 @@ void FizzBuzz(){
     BOOL fizzBuzz = YES;
     
     // Loop through numbers, find multiples of 3 or 5, calculate sum
-    for (int theNum = 1; theNum < maxNum; theNum += 1){
+    for (int theNum = 1; theNum < ceilingNum; theNum += 1){
         
         // Is number a multiple of 3
         if (theNum % fizz == 0 && theNum % buzz == 0) {
@@ -86,4 +86,68 @@ void FizzBuzz(){
     }
     // Print the sum of numbers to the log
     NSLog(@"The sum of the numbers is %d", multiSum);
+}
+
+
+
+
+/*
+ Palmindrome
+ A palindromic number reads the same both ways. The largest palindrome made from the
+ product of two 2-digit numbers is 9009 = 91 x 99. Find the largest palmindrome made from
+ the product of two 3-digit numbers.
+ 
+ String palindrome test cases:
+ mom
+ dad
+ amanaplanacanalpanama
+ 
+ Possible algorithms:
+ 1. Convert number to string, step thorugh chars in reverse order and store in new string. Compare both strings.
+ 2. In case of palindrome only need to compare half of string. Compare first and last, compare second and second from last, until
+ you reach the middle (length/2)
+ 3. Divide number by 10 to get the last digit, set to new number, multiply that by 10. Repeat until through all digits
+*/
+void Palindrome(){
+    
+    // Initialize constants
+    // const int minNum = 100;
+    // const int maxNum = 999;
+    
+    // Declare variables
+    int theProduct;
+    NSMutableString * theProductString = [[NSMutableString alloc]init];
+    NSMutableString * theReverseProductString = [[NSMutableString alloc]init];
+    NSMutableString * palindrome = [[NSMutableString alloc]init];
+    
+    // Calculate product of two 3-digit numbers, assuming that they can be same number
+    // Convert product int to string, read each character from string starting at end, put in new variable
+    // Compare the original string with the reversed version, if same then is palindrome
+    
+    for (int firstFactor = 100; firstFactor < 1000; firstFactor += 1){
+        for (int secondFactor = 100; secondFactor < 1000; secondFactor += 1){
+            [theReverseProductString setString:@""];
+            
+            
+            // Calculate product of two numbers, convert to string
+            theProduct = firstFactor * secondFactor;
+            theProductString = [NSMutableString stringWithFormat:@"%d", theProduct];
+            // NSLog(@"%@", theProductString);
+            
+            // Loop through each char of string in reverse, copy to another string
+            for(NSInteger count = ([theProductString length] -1); count >= 0; count--){
+                [theReverseProductString appendFormat:[NSString stringWithFormat:@"%c", [theProductString characterAtIndex:count]]];
+                
+//                NSLog(@"Character at index %d of string %@", count, theProductString);
+//                NSLog(@"Character: %c", [theProductString characterAtIndex:count]);
+            }
+            
+            // Compare two strings, if equal then we have a palindrome
+            if ([theProductString isEqualToString:theReverseProductString]){
+                [palindrome setString: theProductString];
+            }
+        }
+    }
+    NSLog(@"The largest palindrome is %@", palindrome);
+    
 }
