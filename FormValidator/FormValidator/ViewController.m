b032b437b40c77c971b65e8d6907e4ea1c170b80
slgraff <slgraff@gmail.com>
2016-04-26 13:46:00 -0400

    Implemented code to handle Return key press for each field.

--- FormValidator/FormValidator/ViewController.m
+++ FormValidator/FormValidator/ViewController.m
@@ -48,10 +48,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-// called when 'return' key pressed. return NO to ignore.
+// called when 'return' key pressed. Return NO to ignore.
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    if ([textField isEqual:self.nameTextField]) {
-        // Check number of words in text field
+    
+    if ([textField isEqual:self.nameTextField]) { // Validate name field
         
         if ([self.formValidator validateName: self.nameTextField.text]) {
             [self.nameTextField resignFirstResponder];
@@ -59,22 +59,43 @@
             return YES;
         }
         
-    } else if ([textField isEqual:self.addressTextField]) {
-        return [self.formValidator validateAddress:self.addressTextField.text];
-    } else if ([textField isEqual:self.addressTextField]) {
-        // Call formValidator for city field
-    } else if () {
-        // Call formValidator for state field
-    } else if () {
-        // Call formValidator for zip field
-    } else if () {
-        // Call formValidator for phone field
-    }
+    } else if ([textField isEqual:self.addressTextField]) { // Validate address field
+        if ([self.formValidator validateAddress:self.addressTextField.text]) {
+            [self.addressTextField resignFirstResponder];
+            [self.cityTextField becomeFirstResponder];
+            return YES;
+        }
         
+    } else if ([textField isEqual:self.cityTextField]) { // Validate city field
+        // Call formValidator for city field
+        if ([self.formValidator validateAddress:self.cityTextField.text]) {
+            [self.cityTextField resignFirstResponder];
+            [self.stateTextField becomeFirstResponder];
+            return YES;
+        }
+
+    } else if ([textField isEqual:self.stateTextField]) { // Validate state field
+        // Call formValidator for state field
+        if ([self.formValidator validateAddress:self.stateTextField.text]) {
+            [self.stateTextField resignFirstResponder];
+            [self.zipTextField becomeFirstResponder];
+            return YES;
+        }
+    } else if ([textField isEqual:self.zipTextField]) { // Validate zip field
+        // Call formValidator for zip field
+        if ([self.formValidator validateAddress:self.zipTextField.text]) {
+            [self.cityTextField resignFirstResponder];
+            [self.stateTextField becomeFirstResponder];
+            return YES;
+        }
+    } else if ([textField isEqual:self.phoneTextField]) { // Validate phone field
+        // Call formValidator for phone field
+        // As is last field, do not resign first responder status
+        return [self.formValidator validateAddress:self.phoneTextField.text];
+        return YES;
+        }
     return NO;
 }
-
-
 
 
 @end
