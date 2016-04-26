1affcb01ad44b76582a8cf853b2ff531e1229e48
slgraff <slgraff@gmail.com>
2016-04-26 13:47:55 -0400

    Declared methods for each form validator in FormValidator.h

--- FormValidator/FormValidator/FormValidator.h
+++ FormValidator/FormValidator/FormValidator.h
@@ -13,10 +13,11 @@
 
 // Declaring method
 - (BOOL) validateName:(NSString *)nameString;
-
 - (BOOL) validateAddress:(NSString *)addressString;
-
+- (BOOL) validateCity:(NSString *)cityString;
+- (BOOL) validateState:(NSString *)stateString;
 - (BOOL) validateZip:(NSString *)zipString;
+- (BOOL) validatePhone:(NSString *)phoneString;
 
 
 @end
