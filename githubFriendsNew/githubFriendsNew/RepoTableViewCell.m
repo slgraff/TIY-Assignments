f3733db203568484618f1d8c520087a939327bc3
slgraff <slgraff@gmail.com>
2016-04-26 10:43:24 -0400

    Table was being displayed, but was underneath the left navigation pane.

--- githubFriendsNew/githubFriendsNew/RepoTableViewCell.m
+++ githubFriendsNew/githubFriendsNew/RepoTableViewCell.m
@@ -28,9 +28,13 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+    if (selected) {
+        self.contentView.backgroundColor = [UIColor blueColor];
+    } else {
+        self.contentView.backgroundColor = [UIColor orangeColor];
 
-    // Configure the view for the selected state
-    self.contentView.backgroundColor = [UIColor orangeColor];
+    }
+
 
 }
 
