580471f4f8f087e3a0ab9d609b873440189fd59e
slgraff <slgraff@gmail.com>
2016-05-05 00:29:17 -0400

    Code is working correctly with first test case. Need to test with other strings also.

--- Atbash/Atbash/main.m
+++ Atbash/Atbash/main.m
@@ -25,7 +25,7 @@ int main(int argc, const char * argv[]) {
         NSMutableString *stringToEncode = [NSMutableString stringWithFormat:@"footer"];
         
         // Initialize 'encodedString' to hold our encoded string
-        NSMutableString *encodedString = [@"" mutableCopy];
+        NSMutableString *encodedString = [NSMutableString stringWithCapacity:100];
         
         // Declare arrays for chars a-z, also for ciper chars
         NSArray *azArray = @[@"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h", @"i", @"j", @"k",
@@ -56,100 +56,17 @@ int main(int argc, const char * argv[]) {
                 // Check for 'NSNotFound' returned from indexOfObject, returns NSUInteger
                 if ([azArray indexOfObject:theChar] != NSNotFound) {
                     
-                    // BUG encodedString is getting appended with an address, not a char
-                    [encodedString appendString:[cipherArray objectAtIndex:[azArray indexOfObject:theChar]]];
+                    // Add char from cipherArray to encodedText
+                    [encodedString appendString:[NSString stringWithFormat:@"%@",[cipherArray objectAtIndex:[azArray indexOfObject:theChar]]]];
                 } else {
+                    // If char not found in azArray, append char to encodedString
                     [encodedString stringByAppendingString:theChar];
                 }
             } else {
                 
             }
         }
-        
         NSLog(@"Encoded version of %@ is %@", stringToEncode, encodedString);
-        
-        
-
-        
-//        // Initialize string to hold current character
-//        NSMutableString *currentChar = [[NSMutableString alloc]init];
-//        NSMutableString *newChar = [[NSMutableString alloc]init];
-//
-//        // Initialize variable theIndex
-//        NSInteger theIndex = 0;
-//        
-//        // Convert helloGreeting to all lower case
-//        [helloGreeting setString:[helloGreeting lowercaseString]];
-//        
-//        
-//        for (int charIndex = 0; charIndex < helloGreeting.length; charIndex += 1) {
-//            
-//            // Get current char in helloGreeting string
-//            // NSLog(@"Character at index %d: %C", charIndex, [helloGreeting characterAtIndex:charIndex]);
-//            // NSString *theChar = [helloGreeting characterAtIndex:charIndex];
-//            
-//            // Get the current character from inputString
-//            currentChar = [NSMutableString stringWithFormat:@"%C", [helloGreeting characterAtIndex:charIndex]];
-//            NSLog(@"Current char: %@", currentChar);
-//            
-//            // Get the index of currentChar in plainString
-//            
-//            theIndex = [plainString rangeOfString:currentChar].location;
-//            
-//            if (theIndex != NSNotFound) {
-//                
-//                // If found, look up character at same index in cipherString, add it to outputString
-//                [atbashGreeting stringByAppendingString:[NSMutableString stringWithFormat:@"%C", [cipherString characterAtIndex:theIndex]]];
-//                NSLog(@"%@", atbashGreeting);
-//            } else {
-//                // If error, keep same character, add to outputString
-//                [atbashGreeting stringByAppendingString:currentChar];
-//                NSLog(@"%@", atbashGreeting);
-//            }
-
-            // theIndex = [plainString rangeOfString:currentChar].location;
-            
-            // Check to make sure that character is found in plainString
-//            if(theIndex != NSNotFound) {
-//                // If found, look up character at same index in cipherString, add it to outputString
-//                [atbashGreeting stringByAppendingString:[NSMutableString stringWithFormat:@"%C", [cipherString characterAtIndex:theIndex]]];
-//                NSLog(@"%@", atbashGreeting);
-//
-//            } else {
-//                // If error, keep same character, add to outputString
-//                [atbashGreeting stringByAppendingString:currentChar];
-//                NSLog(@"%@", atbashGreeting);
-//            }
-
-            // Lookup that index in cipherString
-            
-            // [cipherString characterAtIndex:theIndex];
-//            
-//            newChar = [NSMutableString stringWithFormat:@"%C", [cipherString characterAtIndex:theIndex]];
-//            
-//            NSLog(@"plainString char: %@", newChar);
-            
-            
-            //NSLog(@"Character at index 0 of plainString: %C", [plainString characterAtIndex:0]);
-            //NSLog(@"Character at index 0 of cipherString: %C", [cipherString characterAtIndex:0]);
-
-            
-            // NSLog (@"%@", [plainString rangeOfString:[helloGreeting characterAtIndex:charIndex]]);
-            
-            // Lookup character at that index in cipherString
-            
-            // assign to atbashGreeting
-            
-            // NSLog(@"%@", [helloGreeting characterAtIndex:charIndex]);
-            
-            // atbashGreeting = [helloGreeting characterAtIndex:charIndex];
-            
-//        }
-        
-        // NSLog(@"%C", [helloGreeting characterAtIndex:0]);
-        
-        
-
         
     }
     return 0;
