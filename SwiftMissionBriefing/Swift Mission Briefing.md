5b79422c28c6c2dcee32e538255621088c5ac1ac
slgraff <slgraff@gmail.com>
2016-05-10 14:10:54 -0400

    Completed ViewController tasks.

---
+++ SwiftMissionBriefing/Swift Mission Briefing.md
@@ -0,0 +1,32 @@
+## Swift Mission Briefing
+
+### Normal Mode
+#### Storyboard Tasks
+* [x] Add two UITextFields to the top of the view. Have them stretch to 80% of their superview's width.
+* [x] Add a button centered below the textfields. The title should read "Authenticate".
+* [x] Add a centered label below the button - name it greetingLabel.
+* [x] Add a UIImageView and set it to the TIY logo image from your image assets. It should be centered with the rest of the elements.
+
+#### ViewController
+
+* [x] 1. The UI elements should be set to an "empty string" in viewDidLoad of the ViewController class.
+* [x] 2. The background color of the view should change to green on successful authentication of the agent.![]()
+* [x] 2. The greeting label should display "Good evening, Agent [name]", where the agent's last name is pulled from the textfield.
+* [x] 3. The mission briefing should display the information from MissionBriefing.txt (copy-paste, you don't have to read from the file). It also contains a placeholder for the agent's last name.
+
+### Hard Mode
+* [ ] Modify the constraints on the imageview (and possibly other views) to cause the logo to shrink when switching to landscape orientation (the current constraints cause it to float off the bottom of the viewable screen). 
+
+### Sugar on Top
+* [x] Set Agent Name Field to be first responder
+* [x] Enabled 'Secure Text Entry' for the agent password field
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
+* [ ]
---
+++ SwiftMissionBriefing/SwiftMissionBriefing/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  SwiftMissionBriefing
+//
+//  Created by Steve Graff on 5/10/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var agentNameTextField: UITextField!
+    @IBOutlet weak var agentPasswordTextField: UITextField!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var missionGreetingTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize Agent Name, Agent Password and Greeting Label and Greeting Text to be empty
+        agentNameTextField.text = ""
+        agentPasswordTextField.text = ""
+        greetingLabel.text = ""
+        missionGreetingTextView.text = ""
+        
+        // Set Agent Name field to be first responder
+        agentNameTextField.isFirstResponder()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+// MARK: Authenticate Agent
+    @IBAction func authenticateButton(sender: AnyObject) {
+        
+        // Dismiss keyboard when authenticate button tapped
+        if (agentNameTextField.isFirstResponder()) {
+            agentNameTextField.resignFirstResponder()
+        }
+        
+        // Ensure both text fields have contents
+        if (agentNameTextField != "" && agentPasswordTextField != "") {
+            
+            // Grab the agent name, parse out the last name
+            let agentName = agentNameTextField.text
+            let lastName = agentName!.componentsSeparatedByString(" ")[1]
+           
+            // Create the agnet greeting using last name, display in label
+            let agentGreeting = "Good Evening, Agent" + " " + lastName
+            greetingLabel.text = agentGreeting
+            
+            // Use string interpolation to insert agentName into mission greeting text
+            missionGreetingTextView.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
+            
+            // Upon successful login, set background color to green
+            view.backgroundColor = UIColor.greenColor()
+            
+        } else {
+            
+            // Upon failed login, set background color to red
+            view.backgroundColor = UIColor.redColor()
+
+        }
+    }
+}
+
