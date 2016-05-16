37748d928ec37a26a47d4e2d7e34293399bdb142
slgraff <slgraff@gmail.com>
2016-05-16 11:58:08 -0400

    Now using Always Use location, which works. (When In Use doesn't work for me. Why???). Have implemented a popover that now appears when tapping on 'Drop Pin' button, currently blank.

--- DudeWheresMyCar/DudeWheresMyCar/CarLocationViewController.swift
+++ DudeWheresMyCar/DudeWheresMyCar/CarLocationViewController.swift
@@ -12,13 +12,33 @@ import MapKit // Added to allow use of MapKit in app
 
 
 
-class CarLocationViewController: UIViewController, CLLocationManagerDelegate {  // Make VC conform to delegate
+class CarLocationViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UIPopoverPresentationControllerDelegate {  // Make VC conform to delegates for CoreLocation, MapView, UIPopoverPresentationControllerDelegate
 
-    let locationManager = CLLocationManager() // New constant property
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var dropPinButton: UIBarButtonItem!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.title = "Dude, where's my car?"
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Requesting 'When in use' never works in this app. Wtf?
+        // self.locationManager.requestWhenInUseAuthorization()
+        
+        // Requesting 'always' auth always works
+        self.locationManager.requestAlwaysAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        self.mapView.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,30 +46,97 @@ class CarLocationViewController: UIViewController, CLLocationManagerDelegate {  
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func getLocation() {
+    
+    // MARK: Drop Pin
+    @IBAction func dropPinButton(sender: AnyObject) {
         
-        // Ask to get users location
-        let authStatus = CLLocationManager.authorizationStatus()
-        if authStatus == .NotDetermined {
-            locationManager.requestWhenInUseAuthorization()
-            return
-        }
+        self.performSegueWithIdentifier("showPopover", sender: self)
         
         
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
+        // Drop pin on map at current location
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: self.mapView.userLocation.coordinate.latitude, longitude: self.mapView.userLocation.coordinate.longitude)
+//        self.mapView.addAnnotation(annotation)
+        
+       
     }
     
-    // MARK: CLLocationManagerDelegate
     
+    // MARK: Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // Perform specific stuff for our popover
+        if segue.identifier == "showPopover" {
+            
+            let vc = segue.destinationViewController as! UIViewController
+            
+            let controller = vc.popoverPresentationController
+            
+            if controller != nil {
+                controller?.delegate = self
+            }
+        }
+    }
+    
+    
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        
+        return .None
+    }
+    
+    
+    
+    
+//    @IBAction func getLocation() {
+//        
+//        // Ask to get users location
+//        let authStatus = CLLocationManager.authorizationStatus()
+//        if authStatus == .AuthorizedWhenInUse {
+//            print("we have authorization")
+//        } else {
+//            locationManager.requestWhenInUseAuthorization()
+//            return
+//        }
+//        
+//        
+//        // locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+//        locationManager.startUpdatingLocation()
+//    }
+    
+    // MARK: CLLocationManagerDelegates
+    
+    // Did Change Authorization Status
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        print("Location status changed")
+    }
+    
+    // Did Fail With Error
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print("didFailWithError \(error)")
     }
     
+    // Did Update Location
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
+
+        let center = CLLocationCoordinate2D(latitude: newLocation.coordinate.latitude, longitude: newLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
     }
+    
+    
+    // MARK: MKMapView Delegates
+    
+//    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+//        if (annotation is MKUserLocation) {
+//            return nil
+//        }
+//        
+//    }
 }
 
---
+++ DudeWheresMyCar/DudeWheresMyCar/PopoverLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PopoverLocationViewController.swift
+//  DudeWheresMyCar
+//
+//  Created by Steve Graff on 5/16/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+import UIKit
+
+class PopoverLocationViewController: UIViewController {
+
+}
