d8f884f64591493f7df652c9ab4cf5e4daf1ff39
slgraff <slgraff@gmail.com>
2016-05-24 21:24:18 -0400

    Added segue from DiceChoiceVC to DiceCollectionVC, verified is displaying the dice.

--- DieBag/DiceChoiceViewController.swift
+++ DieBag/DiceChoiceViewController.swift
@@ -56,15 +56,15 @@ class DiceChoiceViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    func prepareForSegue(segue: diceCollectionSegue, sender: AnyObject?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+    
     
     // MARK: Get Dice Choices
     func getDiceChoices() {
--- DieBag/DieBag/DCVC+datasource.swift
+++ DieBag/DieBag/DCVC+datasource.swift
@@ -24,24 +24,19 @@ extension DiceCollectionViewController {
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return self.diceHolder.count
+        return diceHolder.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("dieCell", forIndexPath: indexPath)
         
-        let keyArray = Array(self.diceHolder.keys)
-        let selectedDie = self.diceHolder[keyArray[indexPath.row]]
+        let keyArray = Array(diceHolder.keys)
+        let selectedDie = diceHolder[keyArray[indexPath.row]]
         let label = UILabel(frame: cell.bounds)
         label.text = String(selectedDie!.currentValue)
         label.textAlignment = .Center
         
-        //        // a map is all of your elements one at a time
-        //        // similar to iterating through using a for loop
-        //        for subView in cell.contentView.subviews.map { (subView) -> () in
-        //            subView.removeFromSuperview()
-        //        }
         
         for subView in cell.contentView.subviews {
             subView.removeFromSuperview()
