2c92fad17e25114c8c18bc96463aac5362698597
slgraff <slgraff@gmail.com>
2016-05-25 14:00:46 -0400

    Initial commit. Created a CollectionViewController embedded in Navigation Controller. Created class files for People, Planets, Starships and Vehicles.

---
+++ StarWarsANewHIG/People.swift
@@ -0,0 +1,11 @@
+//
+//  People.swift
+//  StarWarsANewHIG
+//
+//  Created by Steve Graff on 5/25/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ StarWarsANewHIG/StarWarsANewHIG/Starships.swift
@@ -0,0 +1,9 @@
+//
+//  Starships.swift
+//  StarWarsANewHIG
+//
+//  Created by Steve Graff on 5/25/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+import Foundation
