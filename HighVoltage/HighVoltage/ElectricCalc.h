68317d7a0de22749b38ec3107a8c0d25899ce02e
slgraff <slgraff@gmail.com>
2016-06-06 15:59:19 -0400

    Created ElectricCalc files for the business logic.

---
+++ HighVoltage/HighVoltage/ElectricCalc.h
@@ -0,0 +1,13 @@
+//
+//  ElectricCalc.h
+//  HighVoltage
+//
+//  Created by Steve Graff on 6/6/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ElectricCalc : NSObject
+
+@end
---
+++ HighVoltage/HighVoltage/ElectricCalc.m
@@ -0,0 +1,13 @@
+//
+//  ElectricCalc.m
+//  HighVoltage
+//
+//  Created by Steve Graff on 6/6/16.
+//  Copyright © 2016 Steve Graff. All rights reserved.
+//
+
+#import "ElectricCalc.h"
+
+@implementation ElectricCalc
+
+@end
