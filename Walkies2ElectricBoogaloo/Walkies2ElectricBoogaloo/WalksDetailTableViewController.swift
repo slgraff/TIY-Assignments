13c1b65dc38e014f2010fe0d3e8490d4a70dc4ea
slgraff <slgraff@gmail.com>
2016-06-15 14:25:27 -0400

    Content now being properly displayed in WalksDetailTableVC. Problem was that numberOfSections and numberOfRows were setting sections and rows to 0. Commented out and now I'm good.

--- Walkies2ElectricBoogaloo/Walkies2ElectricBoogaloo/WalksDetailTableViewController.swift
+++ Walkies2ElectricBoogaloo/Walkies2ElectricBoogaloo/WalksDetailTableViewController.swift
@@ -75,15 +75,15 @@ class WalksDetailTableViewController: UITableViewController {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
