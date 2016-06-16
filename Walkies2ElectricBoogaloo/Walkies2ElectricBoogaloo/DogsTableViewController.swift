212c756643f10e1fd14af684d632f340c6532ae2
slgraff <slgraff@gmail.com>
2016-06-16 15:22:39 -0400

    DogsTableVC now populating with list of dogs from db.

--- Walkies2ElectricBoogaloo/Walkies2ElectricBoogaloo/DogsTableViewController.swift
+++ Walkies2ElectricBoogaloo/Walkies2ElectricBoogaloo/DogsTableViewController.swift
@@ -9,18 +9,78 @@
 import UIKit
 import CoreData
 
+protocol DogPickerDelegate: class {
+    func didSelectDog(dog: Dogs)
+}
 
 class DogsTableViewController: UITableViewController {
     
-    
     var managedObjectContext: NSManagedObjectContext!
-
+    var dogs = [Dogs]()
+    
+    weak var pickerDelegate: DogPickerDelegate?
+    var selectedDog: Dogs?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Dogs"
+        
+        reloadData()
     }
+    
+    func reloadData() {
+        let fetchRequest = NSFetchRequest(entityName: "Dogs")
+        
+        do {
+            if let results = try managedObjectContext.executeFetchRequest(fetchRequest) as? [Dogs] {
+                dogs = results
+                tableView.reloadData()
+            }
+        } catch {
+            fatalError("Error fetching list of dogs!")
+        }
+    }
+    
+    
+    // MARK: Table View data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dogs.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("DogCell", forIndexPath: indexPath)
+        
+        let dog = dogs[indexPath.row]
+        cell.textLabel?.text = dog.name
+        
+        if let selectedDog = selectedDog where selectedDog == dog {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let pickerDelegate = pickerDelegate {
+            let dog = dogs[indexPath.row]
+            selectedDog = dog
+            pickerDelegate.didSelectDog(dog)
+            
+            tableView.reloadData()
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    
+    // MARK: Memory Error Handling
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
